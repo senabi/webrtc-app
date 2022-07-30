51354e78f36455019a9e86d0d765b20edb5d4b6d
mir <sensenabi@gmail.com>
2022-07-30 02:55:38 -0500

    add p2p base

--- src/server/router/room.ts
+++ src/server/router/room.ts
@@ -2,21 +2,9 @@ import { createRouter } from "./context";
 import { EventEmitter } from "events";
 import { Subscription, TRPCError } from "@trpc/server";
 import { Prisma, Room, Peer } from "@prisma/client";
-import { createMediasoupRouter } from "../mediasoup/router";
 import { z } from "zod";
-import { Router } from "mediasoup/node/lib/Router";
-import { RtpCapabilities } from "mediasoup-client/lib/RtpParameters";
-import { createWebrtcTransport } from "../mediasoup/createWebrtcTransport";
-import { Transport } from "mediasoup/node/lib/Transport";
-import { DtlsParameters } from "mediasoup-client/lib/types";
-import { Producer } from "mediasoup/node/lib/Producer";
 
 const ee = new EventEmitter();
-// let mediasoupRouter: Router;
-// createMediasoupRouter().then(router => (mediasoupRouter = router));
-const mediasoupRouter = createMediasoupRouter();
-let producerTransport: Transport;
-let producer: Producer;
 
 export const roomRouter = createRouter()
   // Room
@@ -37,11 +25,6 @@ export const roomRouter = createRouter()
       return await ctx.prisma.room.create({
         data: {},
       });
-    },
-  })
-  .query("get-router-rtp-capabilities", {
-    async resolve({ input, ctx }) {
-      return (await mediasoupRouter).rtpCapabilities;
     },
   })
   .query("get-peers", {
@@ -76,63 +59,6 @@ export const roomRouter = createRouter()
         ee.emit("peers");
         return () => ee.off("peers", peersOnRoom);
       });
-    },
-  })
-  .subscription("get-router-rtp-capabilities", {
-    input: z.object({ peerId: z.string() }),
-    async resolve({ input, ctx }) {
-      return new Subscription<RtpCapabilities>(emit => {
-        // emit.data(cap);
-        // ee.on("router-rtp-capabilities", emit);
-        const onRouterRtpCapabilities = async (peerId: string) => {
-          if (peerId !== input.peerId) return;
-          const cap = (await mediasoupRouter).rtpCapabilities;
-          emit.data(cap);
-        };
-        ee.on("router-rtp-capabilities", onRouterRtpCapabilities);
-        ee.emit("router-rtp-capabilities", input.peerId);
-        return () => ee.off("router-rtp-capabilities", onRouterRtpCapabilities);
-      });
-    },
-  })
-  .mutation("create-producer-transport", {
-    input: z.object({
-      peerId: z.string(),
-      msg: z.string(),
-    }),
-    async resolve({ input, ctx }) {
-      const msg = JSON.parse(input.msg);
-      try {
-        const { transport, params } = await createWebrtcTransport(
-          await mediasoupRouter
-        );
-        producerTransport = transport;
-        return params;
-      } catch (e) {
-        console.error(e);
-      }
-    },
-  })
-  .mutation("connect-producer-transport", {
-    input: z.object({
-      msg: z.string(),
-    }),
-    async resolve({ input, ctx }) {
-      const msg = JSON.parse(input.msg);
-      await producerTransport.connect({ dtlsParameters: msg.dtlsParameters });
-      return "producer connected";
-    },
-  })
-  .mutation("produce", {
-    input: z.object({
-      msg: z.string(),
-    }),
-    async resolve({ input, ctx }) {
-      console.log("producing");
-      const msg = JSON.parse(input.msg);
-      const { kind, rtpParameters } = msg;
-      producer = await producerTransport.produce({ kind, rtpParameters });
-      return { id: producer.id };
     },
   })
   .mutation("add-answer", {
@@ -270,11 +196,6 @@ export const roomRouter = createRouter()
       return new Subscription<Peer>(emit => {
         const onJoin = async (roomId: string, newPeer: Peer) => {
           if (roomId !== input.roomId || newPeer.id === input.peerId) return;
-          // const users = await ctx.prisma.peer.findMany({
-          //   where: {
-          //     roomId: input.roomId,
-          //   },
-          // });
           emit.data(newPeer);
         };
         ee.on("join", onJoin);
@@ -282,20 +203,3 @@ export const roomRouter = createRouter()
       });
     },
   });
-// .subscription("on-other", {
-//   input: z.object({
-//     roomId: z.string(),
-//     peerId: z.string(),
-//   }),
-//   async resolve({ input, ctx }) {
-//     return new Subscription<Peer>(emit => {
-//       const onJoin = async (roomId: string, newPeer: Peer) => {
-//         if (roomId !== input.roomId || newPeer.id === input.peerId) return;
-//         ctx.
-//         emit.data(newPeer);
-//       };
-//       ee.on("other", onJoin);
-//       return () => ee.off("other", emit.data);
-//     });
-//   },
-// });
